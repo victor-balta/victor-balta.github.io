ded5f3065987686d8fcf6ddf311a458a257b8087
Victor <victorbalta@outlook.com>
2026-02-14 14:46:09 +0100

    Update config for main user site

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/victorbalta/', // Set to repo name for project site
+  base: '/', // Set to '/' for User Site (username.github.io)
 })
